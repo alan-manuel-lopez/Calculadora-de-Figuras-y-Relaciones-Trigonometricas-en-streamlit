044aec4c443ef49b1d744bcbe34ae0310a223675
alan-manuel-lopez <a377341@uach.mx>
2025-10-15 08:05:28 -0700

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ if option=="Circulo":
   area=math.pi * radio**2
   perimetro=2*math.pi*radio
   col1, col2, col3 = st.columns(3)
-col1.metric("Radio", radio, "0")
-col2.metric("Area", area, "0")
-col3.metric("Perimetro", perimetro, "0")
+  col1.metric("Radio", radio, "0")
+  col2.metric("Area", area, "0")
+  col3.metric("Perimetro", perimetro, "0")
 if option=="Triangulo":
 # Solicitar al usuario la base y la altura
   base = st.slider("seleccione la longitud de la base", 0.0, 10.0, 5.0) 
