8c84761dfac9c55f31a89ad599f56e971a11dda2
alan-manuel-lopez <a377341@uach.mx>
2025-10-15 07:47:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ if option=="Circulo":
   area=math.pi * radio**2
   perimetro=2*math.pi*radio
   col1, col2, col3 = st.columns(3)
-col1.metric("Radio", "radio", "0")
-col2.metric("Area", "area", "0")
-col3.metric("Perimetro", "perimetro", "0")
+col1.metric("Radio", radio, "0")
+col2.metric("Area", area, "0")
+col3.metric("Perimetro", perimetro, "0")
