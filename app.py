179cc67609f2021cf2788a972be2d110b51d0bdd
alan-manuel-lopez <a377341@uach.mx>
2025-10-15 07:45:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ option=st.selectbox(
   ("Circulo","Triangulo","Rectangulo","Cuadrado"),
 )
 st.write("Eleccion:",option)
+if option=circulo:
+  radio = st.slider("selecciona el radio", 0.0, 10.0, 5.0)
+  area=math.pi * radio**2
+  perimetro=2*math.pi*radio
+  col1, col2, col3 = st.columns(3)
+col1.metric("Radio", "radio", "0")
+col2.metric("Area", "area", "0")
+col3.metric("Perimetro", "perimetro", "0")
