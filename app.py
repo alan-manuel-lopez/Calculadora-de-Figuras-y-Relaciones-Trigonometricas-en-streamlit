f9e53fb0f861022c02f82d767625000e2116be8b
alan-manuel-lopez <a377341@uach.mx>
2025-10-16 16:30:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -19,25 +19,25 @@ if option=="Circulo":
   col2.metric("Area", area1, "0")
   col3.metric("Perimetro", perimetro1, "0")
 # 1. Crea una figura y un eje
-fig, ax = plt.subplots()
+  fig, ax = plt.subplots()
 
 # 2. Crea un círculo. 
 # Parámetros: centro (x, y) y radio (radius)
 # fill=False para que solo dibuje el borde
-circle = patches.Circle((0.5, 0.5), radius=0.3, fill=False)
+  circle = patches.Circle((0.5, 0.5), radius=0.3, fill=False)
 
 # 3. Añade el parche del círculo al eje
-ax.add_patch(circle)
+  ax.add_patch(circle)
 
 # 4. Establece los límites de los ejes para asegurar que el círculo sea visible
-ax.set_xlim(0, 1)
-ax.set_ylim(0, 1)
+  ax.set_xlim(0, 1)
+  ax.set_ylim(0, 1)
 
 # 5. Asegura que el aspecto de los ejes sea igual
-plt.axis('equal')
+  plt.axis('equal')
 
 # 6. Muestra el gráfico
-plt.show()
+  plt.show()
 if option=="Triangulo":
 # Solicitar al usuario la base y la altura
   base = st.slider("seleccione la longitud de la base", 0.0, 10.0, 5.0) 
